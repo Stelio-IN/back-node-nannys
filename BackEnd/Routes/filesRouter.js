a7dd0dca95ef060a7eee5f58713288b405cbc472
Stelio Acácio Mondlane <109701079+Stelio-IN@users.noreply.github.com>
2025-01-22 23:33:41 +0200

    project test

--- BackEnd/Routes/filesRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import FileController from "../Controllers/FilesController.js";
-
-const router = Router();
-
-router.delete("/deletefile/:id", FileController.deleteFile);
-
-export default router;
--- BackEnd/src/types/mpesa-node-api.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'mpesa-node-api' {
-    export function initiate_c2b(amount: number, phoneNumber: string, reference: string): Promise<any>;
-    // Add other methods as needed
-} 
